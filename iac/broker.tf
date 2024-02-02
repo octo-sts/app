bfd1873e43e263c0c26750956800c1ead0b8c1f2
Matt Moore <mattmoor@chainguard.dev>
2024-02-02 18:07:57 -0500

    notifications everywhere (#82)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- iac/broker.tf
+++ iac/broker.tf
@@ -1,18 +1,18 @@
 // Create the Broker abstraction.
 module "cloudevent-broker" {
   source  = "chainguard-dev/common/infra//modules/cloudevent-broker"
-  version = "0.4.3"
+  version = "0.4.4"
 
   name       = "octo-sts-broker"
   project_id = var.project_id
   regions    = module.networking.regional-networks
 
-  // TODO: notification_channels
+  notification_channels = local.notification_channels
 }
 
 module "cloudevent-recorder" {
   source  = "chainguard-dev/common/infra//modules/cloudevent-recorder"
-  version = "0.4.3"
+  version = "0.4.4"
 
   name       = "octo-sts-recorder"
   project_id = var.project_id
@@ -22,6 +22,8 @@ module "cloudevent-recorder" {
   retention-period = 90
 
   provisioner = "serviceAccount:${data.google_client_openid_userinfo.me.email}"
+
+  notification_channels = local.notification_channels
 
   types = {
     "dev.octo-sts.exchange" : {
