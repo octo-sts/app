91d26cc9bc9c5841f61bbb8970a12edba837606a
Matt Moore <mattmoor@chainguard.dev>
2024-02-01 21:56:57 -0800

    Add event recorder with a basic schema.
    We need a better way of representing the custom claims, but I've hard-coded `job_workflow_ref` for now, which is all we currently use.
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- iac/broker.tf
+++ iac/broker.tf
@@ -7,3 +7,19 @@ module "cloudevent-broker" {
   project_id = var.project_id
   regions    = module.networking.regional-networks
 }
+
+module "cloudevent-recorder" {
+  source  = "chainguard-dev/common/infra//modules/cloudevent-recorder"
+  version = "0.4.3"
+
+  name       = "octo-sts-recorder"
+  project_id = var.project_id
+  regions    = module.networking.regional-networks
+  broker     = module.cloudevent-broker.broker
+
+  retention-period = 90
+
+  types = {
+    "dev.octo-sts.exchange": file("${path.module}/sts_exchange.schema.json"),
+  }
+}
