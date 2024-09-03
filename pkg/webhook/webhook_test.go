72fe305db8601920bdb2d250e644926329312cb7
Billy Lynch <1844673+wlynch@users.noreply.github.com>
2024-09-03 17:00:12 -0400

    Fix webhook filter envvar. (#487)
    GITHUB_ORGANIZATION_FILTER -> GITHUB_WEBHOOK_ORGANIZATION_FILTER

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -4,13 +4,23 @@
 package webhook
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/chainguard-dev/clog/slogtest"
 	"github.com/google/go-github/v61/github"
 )
@@ -40,4 +50,80 @@ func TestValidatePolicy(t *testing.T) {
 	if err := validatePolicies(ctx, gh, "foo", "bar", "deadbeef", []string{"policy.json"}); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestOrgFilter(t *testing.T) {
+	gh := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "should not be called", http.StatusUnauthorized)
+	}))
+	defer gh.Close()
+
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tr := ghinstallation.NewAppsTransportFromPrivateKey(gh.Client().Transport, 1234, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tr.BaseURL = gh.URL
+
+	secret := []byte("hunter2")
+	v := &Validator{
+		Transport:     tr,
+		WebhookSecret: [][]byte{secret},
+		Organizations: []string{"foo"},
+	}
+
+	srv := httptest.NewServer(v)
+	defer srv.Close()
+
+	for _, tc := range []struct {
+		org  string
+		code int
+	}{
+		// This fails because the organization is in the filter, so we try to resolve it but it's pointed at a no-op github backend.
+		{"foo", http.StatusInternalServerError},
+		// This passes because the organization is not in the filter, so the server will fast-return a 200.
+		{"bar", http.StatusOK},
+	} {
+		t.Run(tc.org, func(t *testing.T) {
+			body, err := json.Marshal(github.PushEvent{
+				Organization: &github.Organization{
+					Login: github.String(tc.org),
+				},
+				Repo: &github.PushEventRepository{
+					Owner: &github.User{
+						Login: github.String(tc.org),
+					},
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			req, err := http.NewRequest(http.MethodPost, srv.URL, bytes.NewBuffer(body))
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("X-Hub-Signature", signature(secret, body))
+			req.Header.Set("X-GitHub-Event", "push")
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := srv.Client().Do(req.WithContext(slogtest.TestContextWithLogger(t)))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp.StatusCode != tc.code {
+				out, _ := httputil.DumpResponse(resp, true)
+				t.Fatalf("expected %d, got\n%s", tc.code, string(out))
+			}
+		})
+	}
+}
+
+func signature(secret, body []byte) string {
+	mac := hmac.New(sha256.New, secret)
+	mac.Write(body)
+	b := mac.Sum(nil)
+
+	return fmt.Sprintf("sha256=%s", hex.EncodeToString(b))
 }
