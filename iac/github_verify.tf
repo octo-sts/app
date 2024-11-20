4541c6936e4a7645ba3c342daa04bb255c096f4d
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2024-11-20 14:32:08 +0100

    set require_squad to false (#607)

--- iac/github_verify.tf
+++ iac/github_verify.tf
@@ -1,9 +1,9 @@
 resource "google_dns_record_set" "github_verify" {
   managed_zone = google_dns_managed_zone.top-level-zone.name
 
-  name         = "_gh-octo-sts-o.octo-sts.dev."
-  type         = "TXT"
-  ttl          = 300
+  name = "_gh-octo-sts-o.octo-sts.dev."
+  type = "TXT"
+  ttl  = 300
 
   rrdatas = [
     "\"cc539450df\"",
